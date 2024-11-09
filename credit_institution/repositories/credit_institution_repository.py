ada6cbdd18a84c91e09680ddc47a1eab01680061
Christoph Hombach <Christoph.Hombach@2xideas.com>
2024-11-09 18:00:00 +0100

    Change CI

--- credit_institution/repositories/credit_institution_repository.py
+++ credit_institution/repositories/credit_institution_repository.py
@@ -1,4 +1,3 @@
-from django.utils import timezone
 from mt_economic_common.credit_institution.models import (
     CreditInstitutionHub,
     CreditInstitutionStaticSatellite,
@@ -12,7 +11,7 @@ from mt_economic_common.country.models import CountryStaticSatellite
 class CreditInstitutionRepository(MontrekRepository):
     hub_class = CreditInstitutionHub
 
-    def std_queryset(self, **kwargs):
+    def set_annotations(self, **kwargs):
         self.add_satellite_fields_annotations(
             CreditInstitutionStaticSatellite,
             [
@@ -30,14 +29,12 @@ class CreditInstitutionRepository(MontrekRepository):
             ],
         )
         self.rename_field("hub_entity_id", "country_id")
-        return self.build_queryset()
 
     def get_queryset_with_account(self):
-        reference_date = timezone.now()
         self.annotations["account_name"] = get_satellite_from_hub_query(
             "accountstaticsatellite", "account_name", "link_credit_institution_account"
         )
         self.annotations["account_id"] = get_satellite_from_hub_query(
             "accountstaticsatellite", "hub_entity.id", "link_credit_institution_account"
         )
-        return self.std_queryset()
+        return self.receive()
