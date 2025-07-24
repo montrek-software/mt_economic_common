d8d9efd1b8fa1582583255b12c344f78e565f43c
Christoph Hombach <Christoph.Hombach@2xideas.com>
2025-07-24 20:25:22 +0200

    Remove admn tables

--- country/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import CountryHub
-from .models import CountryStaticSatellite
-
-admin.site.register(CountryHub)
-admin.site.register(CountryStaticSatellite)
--- credit_institution/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import CreditInstitutionStaticSatellite
-from .models import CreditInstitutionHub
-
-# Register your models here
-admin.site.register(CreditInstitutionStaticSatellite)
-admin.site.register(CreditInstitutionHub)
